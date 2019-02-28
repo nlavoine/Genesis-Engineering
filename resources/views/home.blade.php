fdc90c24c042b78a2d9cef57a3e19d32d462e1a4
utopik21 <maxime-bouton@outlook.com>
2019-02-28 15:45:41 +0100

    refactor (caroussel) : add images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,18 @@
 @extends('layout.app')
 @section('content')
+    <style>
+        
+    </style>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="/image/razer.jpg" alt="First slide">
+                <img class="d-block w-100" src="{{asset('/assets/image/razer.jpg')}}" alt="First slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/image/razer2.jpg" alt="Second slide">
+                <img class="d-block w-100" src="{{asset('/assets/image/razer2.jpg')}}" alt="Second slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/image/razer3.jpg" alt="Third slide">
+                <img class="d-block w-100" src="{{asset('/assets/image/razer3.jpg')}}" alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
