5fd49728fde96615a76568b55ba8743a56577d6b
utopik21 <maxime-bouton@outlook.com>
2019-02-28 15:35:44 +0100

    caroussel2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,25 @@
 @extends('layout.app')
 @section('content')
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="/image/razer.jpg" alt="Los Angeles">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/image/razer.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/image/razer2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/image/razer3.jpg" alt="Third slide">
+            </div>
         </div>
-
-        <div class="item">
-            <img src="/image/razer2.jpg" alt="Chicago">
-        </div>
-
-        <div class="item">
-            <img src="/image/razer3.jpg" alt="New York">
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 
     <!-- Left and right controls -->
