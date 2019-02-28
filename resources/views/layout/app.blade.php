4eb1a010ac41668690fc5fade414e3bf7a682c84
utopik21 <maxime-bouton@outlook.com>
2019-02-28 15:32:07 +0100

    bootstrap

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="keywords" content="SJ3, Multimédia, matériel,informatique, pc, photo">
         <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-              crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <title>App Name - @yield('title')</title>
     </head>
