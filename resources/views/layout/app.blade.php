7906dedbb4f8f4b4e080664f6ec4152a22135e26
Julien FAURE <julien.faure26@gmail.com>
2019-02-28 15:15:22 +0100

    ajout footer et maj app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,14 @@
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="SJ3, Multimédia, matériel,informatique, pc, photo">
+        <title>S3J Multimédia</title>
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+              crossorigin="anonymous">
+        
         <title>App Name - @yield('title')</title>
     </head>
     <body>
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info">
+                    <ul class="list-unstyled">
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>Accueil</a></li>
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>Qui sommes-nous ?</a></li>
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>Gammes</a></li>
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info">
+                    <ul class="list-unstyled">
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>CGV</a></li>
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>CGU</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info">
+                    <ul class="list-unstyled">
+                        <li><a href="" title=""><i class="fa fa-facebook-square"></i>Facebook</a></li>
+                        <li><a href="" title=""><i class="fa fa-twitter"></i>Twitter</a></li>
+                        <li><a href="" title=""><i class="fa fa-instagram"></i>Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-info">
+                    <ul class="list-unstyled">
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>FR</a></li>
+                        <li><a href="" title=""><i class="fa fa-angle-double-right"></i>UK</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
