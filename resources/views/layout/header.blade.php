3365d39e5b6d0cfbef62b6ed34598313b8ef1de3
nicolas lavoine <nlavoine.dev@gmail.com>
2019-03-04 15:48:49 +0100

    Upadate : Header done (will have to add icons)

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,8 +1,8 @@
 <!--Nav-->
 <header class="sticky-top">
     <div class="container">
-        <div class="row">
-            <ul class="nav text">
+        <div class="row justify-content-end">
+            <ul class="nav">
                 <li class="nav-item">
                     <a class="nav-link text-light" href="#">Mon compte</a>
                 </li>
@@ -10,18 +10,18 @@
                     <a class="nav-link text-light" href="#">Mon panier</a>
                 </li>
                 <li class="nav-item">
-
+                    <a class="nav-link text-light" href="#">Rechercher</a>
                 </li>
             </ul>
 
         </div>
         <div class="row">
-            <nav class="navbar navbar-expand-md">
-                    <a class="navbar-brand" href="index.php">
+            <nav class="navbar navbar-expand-md align-items-end">
+                    <a class="navbar-brand" href="/">
                         <img src="{{asset('/assets/images/logo_final.png')}}" alt="">
                     </a>
 
-                <button class="navbar-toggler order-md-last" type="button" data-toggle="collapse" data-target="#navbar"
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
                         aria-controls="navbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -29,7 +29,7 @@
                 <div class="collapse navbar-collapse" id="navbar">
                     <ul class="navbar-nav justify-content-end mr-auto d-flex flex-fill">
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="index.php">Accueil</a></li>
+                            <a class="nav-link text-light" href="/">Accueil</a></li>
                         <li class="nav-item">
                             <a class="nav-link text-light" href="qsn.php">Qui sommes nous</a></li>
                         <li class="nav-item dropdown">
