3bffd5b4354b368c65ca08553f18a3cc467fbd05
nicolas lavoine <nlavoine.dev@gmail.com>
2019-03-05 10:44:59 +0100

    Fixed duplicated cart template (already created in 'cart' directory, my mystake)

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class CartController extends Controller
 {
     public function index() {
-        return view('cart');
+        return view('cart/index');
     }
 
     public function valid() {
