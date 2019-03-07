e16c9c679e26cbd16f0228e2c6cf70382ba42b87
xkr225 <yn.michel@gmail.com>
2019-03-07 16:23:41 +0100

    add (db) : maj table Users et ajout table Shippings

--- database/migrations/2019_03_07_151405_create_shippings_table.php
+++ database/migrations/2019_03_07_151405_create_shippings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateShippingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shippings', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string ('first_name', 100);
-            $table->string('last_name',100);
-            $table->string('email',75)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 75);
-            $table->rememberToken();
+            $table->string('company',50);
+            $table->string('type',50);
+            $table->decimal('price', 8,2);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shippings');
     }
 }
