7886a9771821519a9f6eb8ab5beb54b7a8bbe60e
xkr225 <yn.michel@gmail.com>
2019-03-08 11:48:55 +0100

    update (table) : maj table products

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-03-04 14:08:35.
+    * Generated on 2019-03-08 10:29:26.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
