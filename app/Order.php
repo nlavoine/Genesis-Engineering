23cbd7cb79b81e4f05e62f43ddb2f749513e2343
Julien FAURE <julien.faure26@gmail.com>
2019-03-08 15:17:46 +0100

    maj relationships tables & create roles tables

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function addressShipping()
+    {
+        return $this->hasOne('app\Address');
+    }
+
+    public function addressReceipt()
+    {
+        return $this->hasOne('app\Address');
+    }
+
+    public function user()
+    {
+        return $this->hasOne('app\User');
+    }
+
+    public function shipping()
+    {
+        return $this->hasOne('app\Shipping');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('app\Product');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $this->hasOne('App\Category');
     }
+
+    public function orders()
+    {
+        return $this->belongsToMany('App\Order');
+    }
 }
